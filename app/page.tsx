ec77604a567b004eacebb0f744b8648d76613913
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-03-05 16:04:28 +0100

    set up tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <p>Det här är startsidan. Här ska alla produkterna visas.</p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="text-amber-800 text-5xl">Det här är startsidan. Här ska alla produkterna visas.</p>
     </main>
   );
 }
