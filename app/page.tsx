38e2efd4d7d73115445b933c64e1c0632183eb60
Simon <simonmansson@live.se>
2025-03-11 18:46:05 +0100

    ProductsPage now renders all products

--- app/page.tsx
+++ app/page.tsx
@@ -5,46 +5,45 @@ import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
 
-
 export default async function Home() {
-    const products = await db.product.findMany();
-    return (
-        <main className="flex min-h-screen flex-col items-center p-10">
-            <h1 className="text-3xl font-bold text-gray-800 mb-6">
-                Welcome to the webshop!
-            </h1>
-            <p className="text-gray-600 mb-8">Here you will find our best products</p>
+  const products = await db.product.findMany();
+  return (
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">
+        Welcome to the webshop!
+      </h1>
+      <p className="text-gray-600 mb-8">Here you will find our best products</p>
 
-            <div className="flex flex-wrap justify-between items-center gap-4">
-                {products.map((products) => (
-                    <div
-                        key={products.articleNumber}
-                        className="border rounded-lg p-4 shadow-md bg-white"
-                    >
-                        <Link href={`/product/${products.articleNumber}/${products.title}`}>
-                            <Image
-                                src={products.image}
-                                alt={products.title}
-                                width={200}
-                                height={150}
-                                className="object-cover w-full h-40 rounded-md cursor-pointer"
-                            />
-                        </Link>
-                        <Link href={`/product/${products.articleNumber}/${products.title}`}>
-                            <h2 className="text-lg font-semibold mt-2 hover:underline">
-                                {products.title}
-                            </h2>
-                        </Link>
-                        <p className="text-gray-700">Price: {products.price} SEK</p>
-                        <Button
-                            onClick={() => toast.success("Added to cart!")}
-                            className="mt-3 w-full cursor-pointer"
-                        >
-                            Buy
-                        </Button>
-                    </div>
-                ))}
-            </div>
-        </main>
-    );
+      <div className="flex flex-wrap justify-between items-center gap-4">
+        {products.map((products) => (
+          <div
+            key={products.articleNumber}
+            className="border rounded-lg p-4 shadow-md bg-white"
+          >
+            <Link href={`/product/${products.articleNumber}/${products.title}`}>
+              <Image
+                src={products.image}
+                alt={products.title}
+                width={200}
+                height={150}
+                className="object-cover w-full h-40 rounded-md cursor-pointer"
+              />
+            </Link>
+            <Link href={`/product/${products.articleNumber}/${products.title}`}>
+              <h2 className="text-lg font-semibold mt-2 hover:underline">
+                {products.title}
+              </h2>
+            </Link>
+            <p className="text-gray-700">Price: {products.price} SEK</p>
+            <Button
+              onClick={() => toast.success("Added to cart!")}
+              className="mt-3 w-full cursor-pointer"
+            >
+              Buy
+            </Button>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,8 +1,48 @@
-export default function ProductsPage() {
-    return (
-        <main className="flex min-h-screen flex-col items-center p-10">
-            <h1 className="text-3xl font-bold text-gray-800 mb-6">Products</h1>
-            <p className="text-gray-600 mb-8">Here you will find our best products</p>
-        </main>
-    );
+"use client"
+import { Button } from "@/components/ui/button";
+import { db } from "@/prisma/db";
+import Link from "next/link";
+import { toast } from "sonner";
+import Image from "next/image";
+
+export default async function ProductsPage() {
+  const products = await db.product.findMany();
+  return (
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">
+        All of our products
+      </h1>
+
+      <div className="flex flex-wrap justify-between items-center gap-4">
+        {products.map((products) => (
+          <div
+            key={products.articleNumber}
+            className="border rounded-lg p-4 shadow-md bg-white"
+          >
+            <Link href={`/product/${products.articleNumber}/${products.title}`}>
+              <Image
+                src={products.image}
+                alt={products.title}
+                width={200}
+                height={150}
+                className="object-cover w-full h-40 rounded-md cursor-pointer"
+              />
+            </Link>
+            <Link href={`/product/${products.articleNumber}/${products.title}`}>
+              <h2 className="text-lg font-semibold mt-2 hover:underline">
+                {products.title}
+              </h2>
+            </Link>
+            <p className="text-gray-700">Price: {products.price} SEK</p>
+            <Button
+              onClick={() => toast.success("Added to cart!")}
+              className="mt-3 w-full cursor-pointer"
+            >
+              Buy
+            </Button>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
