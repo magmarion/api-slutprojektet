c8f767bb7b509d0b1fe545727747d61e122ca6ae
Omar-Alme <omar.almehdar17@gmail.com>
2025-03-11 13:41:34 +0100

    remove folders to fix typo

--- app/Admin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminPage() {
-    return (
-        <div>
-            <h1 className="flex justify-centertext-3xl font-bold">Admin Dashboard</h1>
-        </div>
-    );
-}
--- app/Checkout/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Checkout() {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold">Checkout</h1>
-        </div>
-    );
-}
