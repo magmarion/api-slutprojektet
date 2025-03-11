eb4d7e0ff1853084523843d0593a7ec003a797c5
Omar-Alme <omar.almehdar17@gmail.com>
2025-03-11 14:31:53 +0100

    add description

--- app/product-data.ts
+++ app/product-data.ts
@@ -3,6 +3,7 @@ export const products = [
         id: 1,
         name: "Trådlösa Hörlurar",
         price: 5500,
+        description: "Trådlösa Hörlurar",
         image:
             "https://soundium.se/media/catalog/product/cache/705b44c076ccef27260ea5d687f0f9cb/p/x/px8darkforest.jpg",
     },
@@ -10,6 +11,7 @@ export const products = [
         id: 2,
         name: "Smartklocka",
         price: 2490,
+        description: "Smartklocka",
         image:
             "https://www.elgiganten.se/image/dv_web_D180001280333226/786744/samsung-galaxy-watch-fe-bt-smartwatch-svart.jpg",
     },
@@ -17,6 +19,7 @@ export const products = [
         id: 3,
         name: "Laptop",
         price: 9999,
+        description: "Laptop",
         image:
             "https://www.jbhifi.com.au/cdn/shop/files/749929-Product-0-I-638518544407964431.jpg?v=1718771855",
     },
@@ -24,6 +27,7 @@ export const products = [
         id: 4,
         name: "Elcykel",
         price: 25999,
+        description: "Elcykel",
         image:
             "https://images-cdn.ubuy.co.in/636326cdb789336d02347545-electric-bike-totguard-electric-bike.jpg",
     },
@@ -31,6 +35,7 @@ export const products = [
         id: 5,
         name: "Trådlösa Hörlurar",
         price: 5500,
+        description: "Trådlösa Hörlurar",
         image:
             "https://soundium.se/media/catalog/product/cache/705b44c076ccef27260ea5d687f0f9cb/p/x/px8darkforest.jpg",
     },
@@ -38,6 +43,7 @@ export const products = [
         id: 6,
         name: "Smartklocka",
         price: 2490,
+        description: "Smartklocka",
         image:
             "https://www.elgiganten.se/image/dv_web_D180001280333226/786744/samsung-galaxy-watch-fe-bt-smartwatch-svart.jpg",
     },
@@ -45,6 +51,7 @@ export const products = [
         id: 7,
         name: "Laptop",
         price: 9999,
+        description: "Laptop",
         image:
             "https://www.jbhifi.com.au/cdn/shop/files/749929-Product-0-I-638518544407964431.jpg?v=1718771855",
     },
@@ -52,6 +59,7 @@ export const products = [
         id: 8,
         name: "Elcykel",
         price: 25999,
+        description: "Elcykel",
         image:
             "https://images-cdn.ubuy.co.in/636326cdb789336d02347545-electric-bike-totguard-electric-bike.jpg",
     },
@@ -59,6 +67,7 @@ export const products = [
         id: 9,
         name: "iPhone 15 pro",
         price: 10999,
+        description: "iPhone 15 pro",
         image:
             "https://www.tashicell.com/themes/tashicell/assets/phones/iPhone%2015%20Pro%20Max%20256GB_2023-09-22.png",
     },
@@ -66,6 +75,7 @@ export const products = [
         id: 10,
         name: "Samsung s24 Ultra",
         price: 8999,
+        description: "Samsung s24 Ultra",
         image:
             "https://www.elgiganten.se/image/dv_web_D1800010021349704/575790/samsung-galaxy-s23-ultra-5g-smartphone-12512gb-gron.jpg",
     },
