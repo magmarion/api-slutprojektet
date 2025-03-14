10e52f86fecb38ad3778358c53a861044adf65a2
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-03-14 23:25:12 +0100

    refactor AddToCartButton to use useCartStore for adding items to cart

--- components/buttons/AddToCartButton.tsx
+++ components/buttons/AddToCartButton.tsx
@@ -1,13 +1,28 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import useCartStore from "@/stores/cartStore";
 import { toast } from "sonner";
 
-export default function AddToCartButton() {
+interface AddToCartButtonProps {
+  id: string;
+  title: string;
+  price: number;
+}
+
+export default function AddToCartButton({ id, title, price }: AddToCartButtonProps) {
+
+  const { addToCart } = useCartStore();
+
+  const handleAddToCart = () => {
+    addToCart({ id, title, price, quantity: 1 });
+    toast.success("Added to cart!");
+  };
+
   return (
     <Button
       data-cy="product-buy-button"
-      onClick={() => toast.success("Added to cart!")}
+      onClick={handleAddToCart}
       className="mt-3 w-full cursor-pointer "
     >
       Buy
