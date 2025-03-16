35c14f68e45e3435a6ce0f47e092cdebba8a43d2
Omar-Alme <omar.almehdar17@gmail.com>
2025-03-16 23:14:43 +0100

    remove admin flow

---
+++ app/admin/product/[articleNumber]/page.tsx
@@ -0,0 +1,7 @@
+export default function EditProductPage() {
+    return (
+        <div className="p-4 md:p-10">
+            <h1 className="text-2xl font-bold mb-6">Edit Product</h1>
+        </div>
+    );
+}
