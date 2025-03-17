bd0f7679732b8f548786f720bf2c6cf37ef6d596
Simon <simonmansson@live.se>
2025-03-18 00:41:11 +0100

    socials footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,15 @@
+import { SocialIcon } from "react-social-icons";
+
 export default function Footer() {
   return (
-    <footer className="p-4 bg-gray-100 text-center">
-      <p>© 2024 The Webshop</p>
+    <footer className="p-4 bg-gray-100">
+      <div className=" gap-2 flex items-center justify-center ">
+        <SocialIcon url="https://youtube.com" />
+        <SocialIcon url="https://facebook.com" />
+        <SocialIcon url="https://instagram.com" />
+        <SocialIcon url="https://pinterest.com" />
+        <p>© 2025 TECHGEAR</p>
+      </div>
     </footer>
   );
 }
