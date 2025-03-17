1348ef968dd023f1e067857053f2e05f353b284f
Omar-Alme <omar.almehdar17@gmail.com>
2025-03-17 18:08:28 +0100

    refractor checkout form into seperate component

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,50 +1,13 @@
 "use client";
+import CheckoutForm from "@/components/checkout/CheckoutForm";
 import useCartStore from "@/stores/cartStore";
-import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
 import { FaMinus, FaPlus } from "react-icons/fa";
-import { z } from "zod";
 
-const checkoutSchema = z.object({
-    name: z.string().min(1, { message: "Name is required" }),
-    email: z
-        .string()
-        .min(1, { message: "Email is required" })
-        .email("Email is invalid"),
-    address: z.string().min(1, { message: "Address is required" }),
-    zip: z.number().min(1, "Zip is required"),
-    country: z.string().min(1, { message: "Country is required" }),
-    city: z.string().min(1, { message: "City is required" }),
-    phone: z.string().min(1, "Phone number is invalid"),
-    cardNumber: z.string().regex(/^\d{16}$/, "Card number is invalid"),
-    expirationDate: z
-        .string()
-        .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, "Invalid format, ex. 12/24"),
-    cvc: z.string().regex(/^\d{3,4}$/, "CVC needs to be 3 or 4 digits"),
-});
-
-type CheckoutFormData = z.infer<typeof checkoutSchema>;
 
 export default function CheckoutPage() {
     const { cartItems, totalPrice, increaseQuantity, decreaseQuantity } = useCartStore();
 
-    // 3. Använd zodResolver för att koppla schemat till React Hook Form
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<CheckoutFormData>({
-        resolver: zodResolver(checkoutSchema),
-    });
-
-    // 4. Hantera "submit"
-    const router = useRouter();
-    const onSubmit = (data: CheckoutFormData) => {
-        console.log("Formulärdata:", data);
-        router.push("/checkout/confirmation");
-    };
 
     return (
         <div className="flex flex-col lg:flex-row justify-evenly items-start w-full p-4">
@@ -121,151 +84,7 @@ export default function CheckoutPage() {
 
 
             <div className="w-full lg:w-1/3">
-                <form onSubmit={handleSubmit(onSubmit)} data-cy="customer-form" className="flex flex-col">
-                    {/* Namn */}
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Name</label>
-                        <input
-                            type="text"
-                            data-cy="customer-name"
-                            {...register("name")}
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.name && (
-                            <p data-cy="customer-name-error" className="text-red-500 text-sm mt-1">{errors.name.message}</p>
-                        )}
-                    </div>
-
-                    {/* E-post */}
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">E-mail</label>
-                        <input
-                            type="email"
-                            data-cy="customer-email"
-                            {...register("email")}
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.email && (
-                            <p data-cy="customer-email-error" className="text-red-500 text-sm mt-1">{errors.email.message}</p>
-                        )}
-                    </div>
-
-                    {/* Telefonnummer */}
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Phone</label>
-                        <input
-                            type="tel"
-                            {...register("phone")}
-                            data-cy="customer-phone"
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.phone && (
-                            <p data-cy="customer-phone-error" className="text-red-500 text-sm mt-1">{errors.phone.message}</p>
-                        )}
-                    </div>
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Address</label>
-                        <input
-                            type="text"
-                            {...register("address")}
-                            data-cy="customer-address"
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.address && (
-                            <p data-cy="customer-address-error" className="text-red-500 text-sm mt-1">{errors.address.message}</p>
-                        )}
-                    </div>
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Zip code</label>
-                        <input
-                            type="text"
-                            {...register("zip")}
-                            data-cy="customer-zipcode"
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.zip && (
-                            <p data-cy="customer-zipcode-error" className="text-red-500 text-sm mt-1">{errors.zip.message}</p>
-                        )}
-                    </div>
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Country</label>
-                        <input
-                            type="text"
-                            {...register("country")}
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.country && (
-                            <p className="text-red-500 text-sm mt-1">{errors.country.message}</p>
-                        )}
-                    </div>
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">City</label>
-                        <input
-                            type="text"
-                            {...register("city")}
-                            data-cy="customer-city"
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.city && (
-                            <p data-cy="customer-city-error" className="text-red-500 text-sm mt-1">{errors.city.message}</p>
-                        )}
-                        <div className="w-full md:w-[50%] flex justify-center items-center mt-4">
-                            <img className="pt-15" src="/Credit-Card-Icons.png" alt="credit cards" />
-                        </div>
-                    </div>
-
-                    {/* Kortnummer */}
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Card number</label>
-                        <input
-                            type="text"
-                            {...register("cardNumber")}
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.cardNumber && (
-                            <p className="text-red-500 text-sm mt-1">
-                                {errors.cardNumber.message}
-                            </p>
-                        )}
-                    </div>
-
-                    {/* Utgångsdatum */}
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Expiration (MM/YY)</label>
-                        <input
-                            type="text"
-                            placeholder="MM/YY"
-                            {...register("expirationDate")}
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.expirationDate && (
-                            <p className="text-red-500 text-sm mt-1">
-                                {errors.expirationDate.message}
-                            </p>
-                        )}
-                    </div>
-
-                    {/* CVC */}
-                    <div className="mb-6">
-                        <label className="block font-medium mb-1">CVC</label>
-                        <input
-                            type="text"
-                            {...register("cvc")}
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        {errors.cvc && (
-                            <p className="text-red-500 text-sm mt-1">{errors.cvc.message}</p>
-                        )}
-                    </div>
-
-                    {/* Submit-knapp */}
-                    <button
-                        type="submit"
-                        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
-                    >
-                        Confirm
-                    </button>
-                </form>
+                <CheckoutForm />
             </div>
         </div>
     );
