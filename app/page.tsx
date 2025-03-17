166c3c898442f406156abd035ceaeea0c8d01d1f
Simon <simonmansson@live.se>
2025-03-17 16:45:24 +0100

    styling and info button

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AddToCartButton from "@/components/buttons/AddToCartButton";
+import { Button } from "@/components/ui/button";
 import { db } from "@/prisma/db";
 import Image from "next/image";
 import Link from "next/link";
@@ -16,24 +17,23 @@ export default async function Home() {
           className="object-center object-cover"
         />
         <div className="absolute inset-0 font-bold text-gray-800 flex flex-col justify-center ml-2 text-sm md:text-3xl lg:text-4xl lg:pl-30 ">
-          <p >Tech You Need.</p>
+          <p>Tech You Need.</p>
           <p className="lg:mt-3">When You Need.</p>
 
           <p className="text-[0.7rem] md:mt-1 md:text-[1rem]">✅ In stock</p>
-          <p className="text-[0.7rem] md:mt-1 md:text-[1rem]">🚛 Fast delivery</p>
+          <p className="text-[0.7rem] md:mt-1 md:text-[1rem]">
+            🚛 Fast delivery
+          </p>
         </div>
       </section>
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">
-        Welcome to the webshop!
-      </h1>
-      <p className="text-gray-600 mb-8">Here you will find our best products</p>
+      <h1 className="text-xl font-bold text-gray-800 m-6">Trending Products</h1>
 
-      <div className="flex flex-wrap justify-between items-center gap-4">
+      <div className="flex flex-col w-full md:flex-row md:flex-wrap ">
         {products.map((product) => (
           <div
             data-cy="product"
             key={product.articleNumber}
-            className="border rounded-lg p-4 shadow-md bg-white"
+            className="border rounded-lg p-4 shadow-md bg-white flex md:flex-col "
           >
             <Link href={`/product/${product.articleNumber}/${product.title}`}>
               <Image
@@ -41,26 +41,35 @@ export default async function Home() {
                 alt={product.title}
                 width={200}
                 height={150}
-                className="object-cover w-full h-40 rounded-md cursor-pointer"
+                className="object-cover w-[150px] h-[150px] rounded-md cursor-pointer"
               />
             </Link>
-            <Link href={`/product/${product.articleNumber}/${product.title}`}>
-              <h2
-                data-cy="product-title"
-                className="text-lg font-semibold mt-2 hover:underline"
-              >
-                {product.title}
-              </h2>
-            </Link>
-            <p data-cy="product-price" className="text-gray-700">
-              Price: {product.price} SEK
-            </p>
-            <AddToCartButton
-              id={product.id}
-              title={product.title}
-              price={product.price}
-              image={product.image}
-            />
+            <div className=" flex flex-col justify-between items-center">
+              <Link href={`/product/${product.articleNumber}/${product.title}`}>
+                <h2
+                  data-cy="product-title"
+                  className="text-lg font-semibold mt-2 hover:underline"
+                >
+                  {product.title}
+                </h2>
+                <p>Apple</p>
+              </Link>
+              <p data-cy="product-price" className="text-gray-700">
+                Price: {product.price} SEK
+              </p>
+              <AddToCartButton
+                id={product.id}
+                title={product.title}
+                price={product.price}
+                image={product.image}
+              />
+
+              <Link href={`/product/${product.articleNumber}/${product.title}`}>
+                <Button className="bg-slate-500">
+                    Info
+                </Button>
+              </Link>
+            </div>
           </div>
         ))}
       </div>
