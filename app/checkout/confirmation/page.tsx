cd493c20b02584325a1a008956de11b643bde024
Rich-Brandon <richbrand2@yahoo.com>
2025-03-18 09:22:28 +0100

    confirmation page commits

--- app/checkout/confirmation/page.tsx
+++ app/checkout/confirmation/page.tsx
@@ -12,3 +12,21 @@ import {
   } from "@/components/ui/card";
   import { Button } from "@/components/ui/button";
   
+
+  export default function ConfirmationPage() {
+    const { cartItems, totalPrice } = useCartStore();
+  
+    return (
+      <div
+        data-cy="confirmation-page"
+        className="flex flex-col lg:flex-row justify-evenly items-start w-full p-4 gap-8"
+      >
+        {/* LEFT SIDE: Thank you message + Order summary */}
+      <Card className="w-full lg:w-1/2 lg:pr-4">
+        <CardHeader>
+          <CardTitle>Thank you for your purchase!</CardTitle>
+          <CardDescription>
+            We appreciate your order. A confirmation email has been sent.
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
