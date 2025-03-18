51cbc5201dd7f62d05f239b432a21559a8524c62
Rich-Brandon <richbrand2@yahoo.com>
2025-03-18 09:20:56 +0100

    confirmation page commits

---
+++ app/checkout/confirmation/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { useCartStore } from "@/stores/cartStore";
+import Image from "next/image";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+  import { Button } from "@/components/ui/button";
+  
