270728f6b03e41bf1ac5496352a6d481007c6570
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-03-20 15:30:58 +0100

    fix: correct typo in main heading text

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
         <main className="flex min-h-screen flex-col items-center bg-slate-200">
             < HeroSection />
             <h1 className="text-2xl font-bold text-center text-gray-900 m-6">
-                supah supah hot deals right now
+                supah dupah hot deals right now
             </h1>
 
             <div className="grid grid-cols-1 px-8 py-4 pb-11 gap-3 md:grid-cols-3 lg:grid-cols-4">
