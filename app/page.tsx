ecd3178a9e45a4b322869fe1fdc11beff0520d9a
Omar-Alme <omar.almehdar17@gmail.com>
2025-03-20 12:38:44 +0100

    refactor: simplify Home component by removing pagination logic and fetching all products

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
 import HeroSection from "@/components/HeroSection";
 import ProductCard from "@/components/products/ProductCard";
-import { Button } from "@/components/ui/button";
 import { db } from "@/prisma/db";
-import Link from "next/link";
 
-export default async function Home({
-    searchParams,
-}: {
-    searchParams: { page?: string };
-}) {
-    const pageSize = 15;
-    // Läs av nuvarande sida från query-parametrarna, defaulta till 1 om inget anges
-    const currentPage = parseInt(searchParams.page ?? "1", 10);
-    const skip = (currentPage - 1) * pageSize;
+export default async function Home() {
 
     // Hämta bara de produkter som ska visas på den aktuella sidan
-    const products = await db.product.findMany({
-        skip,
-        take: pageSize,
-    });
+    const products = await db.product.findMany();
 
-    // Om du vill ha totala antalet produkter för att kunna räkna ut antalet sidor:
-    const totalProducts = await db.product.count();
-    const totalPages = Math.ceil(totalProducts / pageSize);
     return (
         <main className="flex min-h-screen flex-col items-center bg-slate-200">
             < HeroSection />
@@ -36,19 +20,8 @@ export default async function Home({
                 ))}
             </div>
 
-            {/* Pagination-länkar */}
-            <div className="flex gap-4 my-8">
-                {currentPage > 1 && (
-                    <Link href={`/?page=${currentPage - 1}`}>
-                        <Button className="rounded-xs cursor-pointer">Previous</Button>
-                    </Link>
-                )}
-                {currentPage < totalPages && (
-                    <Link href={`/?page=${currentPage + 1}`}>
-                        <Button className="rounded-xs cursor-pointer ">Next</Button>
-                    </Link>
-                )}
-            </div>
+    
         </main>
     );
 }
+
