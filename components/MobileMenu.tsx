5479ee586e6602b5222d3329c9f3db409321ac61
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-03-20 12:38:43 +0100

    feat: enhance MobileMenu with active link highlighting and improve Header component structure

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface MobileMenuProps {
     isOpen: boolean;
@@ -7,6 +8,8 @@ interface MobileMenuProps {
 }
 
 export default function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
+    const pathname = usePathname(); // Get the current route
+
     return (
         <div
             className={`fixed top-0 left-0 w-full h-full bg-slate-900 bg-opacity-95 z-50 flex flex-col items-center justify-center transition-transform duration-300 ${isOpen ? "translate-x-0" : "-translate-x-full"
@@ -16,17 +19,28 @@ export default function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
                 ×
             </button>
 
+            <Link href="/" onClick={onClose} className="flex flex-col items-center mb-8">
+                <p className="text-white text-2xl font-bold tracking-wide">tech</p>
+                <p className="text-white text-2xl font-extrabold -mt-2">gear</p>
+            </Link>
+
             <nav className="flex flex-col gap-6 text-center">
-                {["home", "product", "about", "contact"].map((item) => (
-                    <Link
-                        key={item}
-                        href={`/${item === "home" ? "" : item}`}
-                        className="text-white text-xl hover:text-gray-400"
-                        onClick={onClose}
-                    >
-                        {item}
-                    </Link>
-                ))}
+                {["home", "product", "about", "contact"].map((item) => {
+                    const linkPath = `/${item === "home" ? "" : item}`;
+                    const isActive = pathname === linkPath;
+
+                    return (
+                        <Link
+                            key={item}
+                            href={linkPath}
+                            className={`text-white text-xl hover:text-gray-400 ${isActive ? "font-bold border-b-2 border-white" : ""
+                                }`}
+                            onClick={onClose}
+                        >
+                            {item}
+                        </Link>
+                    );
+                })}
             </nav>
         </div>
     );
