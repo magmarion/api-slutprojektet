abbb30956057e29ad637e61537eff690f21af1af
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-05-19 13:46:50 +0200

    create migration and seed files for User and Product tables; update seed logic for user data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,15 +3,19 @@ import { db } from "./client";
 import { products as productData } from "../data/index";
 
 async function main() {
-    await db.user.upsert({
+    const user = await db.user.findUnique({
         where: { email: "cr7@mail.com" },
-        create: { name: "Cristiano", email: "cr7@mail.com" },
-        update: {
-            name: "Lionel Messi",
-            email: "lm10@gmail.com",
-        }
     });
 
+    if (user) {
+        await db.user.update({
+            where: { id: user.id },
+            data: {
+                name: "Lionel Messi",
+                email: "lm10@gmail.com",
+            },
+        });
+    }
     // Seed products
     for (const product of productData) {
         await db.product.create({
