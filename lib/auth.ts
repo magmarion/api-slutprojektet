ea498052b7a398d8b3dd0bb8a813fbd65cf83ded
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-05-20 11:11:09 +0200

    implement Better Auth integration; update environment variables, add auth routes, and modify Prisma schema for user sessions and accounts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,19 @@
-// Autentisering med NextAuth.js
+// Autentisering med Better Auth
 
-// Här ska NextAuth.js konfigureras och säkerställas att användare måsta logga in innan de kan göra beställningar eller se sina tidigare köp.
+// Här ska Better Auth konfigureras och säkerställas att användare måsta logga in innan de kan göra beställningar eller se sina tidigare köp.
 
+import { db } from "@/prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(db, {
+        provider: "postgresql",
+    }),
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+    },
+});
