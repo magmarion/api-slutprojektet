2a0f3526453c88f3c3b9cbb8d3680e7edffe8e99
Simon <simonmansson@live.se>
2025-05-21 21:23:43 +0200

    re added const user after acc. removing it. Added control to check if product.category is missing, updated editproductform with categories[]

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,39 +1,58 @@
+import { products as productData } from "@/data/index";
 import { db } from "./client";
-import { products as productData } from "../data/index";
 
 async function main() {
-  for (const product of productData) {
-    // Skapa eller hitta alla kategorier för produkten
-    const categoryRecords = [];
-    for (const cat of product.category) {
-      const category = await db.category.upsert({
-        where: { name: cat },
-        update: {},
-        create: {
-          name: cat,
-          category: cat,
-        },
-      });
-      categoryRecords.push({ id: category.id });
-    }
+  const user = await db.user.findUnique({
+    where: { email: "cr7@mail.com" },
+  });
 
-    // Skapa produkten och koppla till alla kategorier
-    await db.product.create({
+  if (user) {
+    await db.user.update({
+      where: { id: user.id },
       data: {
-        id: product.id || undefined,
-        articleNumber: product.articleNumber,
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        image: product.image,
-        categories: {
-          connect: categoryRecords,
-        },
+        name: "Lionel Messi",
+        email: "lm10@gmail.com",
       },
     });
-  }
 
-  console.log("Seed completed");
+    for (const product of productData) {
+      // Skapa eller hitta alla kategorier för produkten
+      const categoryRecords = [];
+
+      if (!product.category || product.category.length === 0) {
+        console.warn(`Product ${product.title} has no categories.`);
+        continue;
+      }
+      
+      for (const cat of product.category) {
+        const category = await db.category.upsert({
+          where: { name: cat },
+          update: {},
+          create: {
+            name: cat,
+          },
+        });
+        categoryRecords.push({ id: category.id });
+      }
+
+      // Skapa produkten och koppla till alla kategorier
+      await db.product.create({
+        data: {
+          id: product.id || undefined,
+          articleNumber: product.articleNumber,
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          image: product.image,
+          categories: {
+            connect: categoryRecords,
+          },
+        },
+      });
+    }
+
+    console.log("Seed completed");
+  }
 }
 
 main()
