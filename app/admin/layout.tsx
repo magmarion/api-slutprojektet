ad3c53c6c25a2c8b750adecf3630d3b554744e67
Malishaah <melisarsh@gmail.com>
2025-05-25 22:52:24 +0200

    frontend/ order

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,25 +1,25 @@
 // app/admin/layout.tsx
-import AdminTable from "@/components/admin/AdminTable";
-import { Button } from "@/components/ui/button";
-import { db } from "@/prisma/client";
-import Link from "next/link";
+import Link from 'next/link';
 
-export default async function AdminPage() {
-  const products = await db.product.findMany({
-    include: { categories: true }, 
-  });
-
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="p-4 md:p-10">
-      <div className="flex items-center justify-between mb-6">
-        <h1 className="text-2xl font-bold">ADMIN DASHBOARD</h1>
-        <Link href="/admin/product/new">
-          <Button className="cursor-pointer" data-cy="admin-add-product">
-            Add Product
-          </Button>
-        </Link>
-      </div>
-      <AdminTable products={products} />
+    <div className='p-4 md:p-10'>
+      <header className='mb-6 flex justify-between items-center'>
+        <h1 className='text-3xl font-bold'>Admin Dashboard</h1>
+        <nav className='space-x-4'>
+          <Link href='/admin' className='underline'>
+            Produkter
+          </Link>
+          <Link href='/admin/orders' className='underline'>
+            Ordrar
+          </Link>
+        </nav>
+      </header>
+      <main>{children}</main>
     </div>
   );
 }
