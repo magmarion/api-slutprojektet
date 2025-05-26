6eda554d3ec20936a253e80dab4923db1723439b
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-05-26 21:27:50 +0200

    Refactor layout to remove unused font imports and streamline styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,24 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Toaster } from "@/components/ui/sonner";
-import { Bangers, Inter, Roboto, Quantico } from "next/font/google";  // Import Quantico
+import { Raleway, Dancing_Script } from "next/font/google";  // Import Quantico
 import type { Metadata } from "next/types";
 import { PropsWithChildren } from "react";
 import "../app/global.css";
 
-const bangers = Bangers({
-  weight: ["400"],
-  subsets: ["latin"],
-  display: "swap",
-});
 
-const inter = Inter({ subsets: ["latin"] });
-
-const roboto = Roboto({
-  weight: ["100", "300", "400", "500", "700", "900"],
-  subsets: ["latin"],
-});
-
-const quantico = Quantico({
+const raleway = Raleway({
   weight: ["400", "700"],  // Specify available weights
   subsets: ["latin"],
   display: "swap",
 });
+
+const dancingScript = Dancing_Script({
+  weight: ["400", "700"],  // Specify available weights
+  subsets: ["latin"],
+  display: "swap",
+});
+
 
 /* Metadata for SEO */
 export const metadata: Metadata = {
@@ -34,7 +29,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={`flex flex-col min-h-screen ${quantico.className}`}>
+      <body className={`flex flex-col min-h-screen ${raleway.className}`}>
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
