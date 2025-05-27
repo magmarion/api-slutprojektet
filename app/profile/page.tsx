959a666dd0eb990aec2c338a3f505f69f1b79206
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-05-27 20:43:09 +0200

    Update Trust Badge color in ProductDetail and ProfilePage components

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -60,7 +60,7 @@ export default async function ProfilePage() {
                 ))}
               </ul>
             ) : (
-              <div className="bg-gradient-to-b from-[#FEFAE1] to-[#daa400] text-[#616F47] p-4 rounded text-center">
+              <div className="bg-gradient-to-b from-[#FEFAE1] to-[#daa400] text-[#223500] p-4 rounded text-center">
                 <FaBoxOpen className="w-16 h-16 mx-auto mb-4" />
                 Du har inte gjort några beställningar ännu.
               </div>
@@ -69,7 +69,7 @@ export default async function ProfilePage() {
         </div>
 
         {/* Trust Badge */}
-        <div className="mt-8 text-center text-[#616F47]">
+        <div className="mt-8 text-center text-[#223500]">
           <p className="inline-flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
