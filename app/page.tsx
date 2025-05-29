b99d1caf37be87f8534163810a90fc0a73b366b3
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-05-29 17:49:15 +0200

    Add UnderHeader component to RootLayout and clean up comments in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,16 @@
 // app/page.tsx
-
 import HeroSection from "@/components/HeroSection";
 import ProductCard from "@/components/products/ProductCard";
 import { db } from "@/prisma/client";
 
 export default async function Home() {
-    // Hämta unika kategorier från databasen
-    const categories = await db.category.findMany({
-        select: { name: true },
-    });
-
-    // Hämta alla produkter
-    const products = await db.product.findMany({
-        include: { categories: true },
-    });
+    const categories = await db.category.findMany({ select: { name: true }, });
+    const products = await db.product.findMany({ include: { categories: true }, });
 
     return (
         <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#FEFAE1] to-[#daa400]">
             <HeroSection />
+
             <h1 className="text-3xl font-bold text-center text-[#594100] m-6">
                 Gröna drömmar blir verklighet hos oss! Köp nu våra blommor för fan!
             </h1>
