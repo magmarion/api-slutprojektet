ec0aabbb366671f26afc909a75f4c49e26bbf8d6
Peter <peter.warholm@gmail.com>
2025-05-30 14:41:47 +0200

    Redirect admin route to admin/dashboard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,37 +1,6 @@
 // app/admin/page.tsx
-import AddProductForm from "@/components/admin/AddProductForm";
-import AdminProductsGrid from "@/components/admin/AdminTable";
-import { getAllProducts } from "@/app/admin/actions";
-import { requireAdminSession } from "@/lib/requiredSession";
+import { redirect } from "next/navigation";
 
-export default async function AdminPage() {
-  // Använd direkt utan att lagra i variabel om du inte behöver data från sessionen
-  await requireAdminSession();
-
-  const products = await getAllProducts();
-
-  return (
-    <main className="p-6 max-w-7xl mx-auto">
-      <h1 className="text-3xl font-bold text-center text-[#3D5300] mb-8">
-        Admin Panel
-      </h1>
-
-      {/* Product Creation Form */}
-      <section className="mb-10">
-        <AddProductForm />
-      </section>
-
-      {/* Product List */}
-      <section>
-        <h2 className="text-2xl font-semibold text-[#3D5300] mb-4">
-          Alla Produkter
-        </h2>
-        {products.length > 0 ? (
-          <AdminProductsGrid products={products} />
-        ) : (
-          <p className="text-gray-500">Inga produkter hittades.</p>
-        )}
-      </section>
-    </main>
-  );
+export default function AdminPage() {
+  redirect("/admin/dashboard");
 }
