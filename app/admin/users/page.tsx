3ae3db93b263ad881e66b4451af7ac43260bd2ad
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-06-04 10:31:53 +0200

    Update heading in AddProductPage to reflect current state of user management

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,7 +1,7 @@
 export default function AddProductPage() {
     return (
         <div className="p-4 md:p-10">
-            <h1 className="text-2xl font-bold mb-6">Hantera användare</h1>
+            <h1 className="text-2xl font-bold mb-6">There should be "users" here but they are not currently found.</h1>
         </div>
     );
 }
